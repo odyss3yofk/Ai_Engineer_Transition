7a623085b40a6456b1c0b133b4edaa9af8f158f8
Kuldeep Bora <kuldeepbora.k@gmail.com>
2026-07-29 23:15:44 +0530

    added assignment5

---
+++ Python_Practice/Python_Fundamentals_2/Assignment5.py
@@ -0,0 +1,19 @@
+# Write a function to return the sum of the digits of a number n
+
+def digits_sum(num):
+
+    a = 0
+    sum = 0
+
+    while num > 0:
+
+        a = num % 10
+
+        num = num//10
+
+        sum += a
+
+    return sum
+
+
+print(digits_sum(121))
