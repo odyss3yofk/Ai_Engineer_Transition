bb0e8811ed4e98962d91ea03ceabe23be6103869
Kuldeep Bora <kuldeepbora.k@gmail.com>
2026-07-30 16:38:29 +0530

    added assignment 10. Fundamentals 2 assignment completed

---
+++ Python_Practice/Python_Fundamentals_2/Assignment10.py
@@ -0,0 +1,16 @@
+# Number guessing game
+
+sec_num = 55
+
+num = int(input("enter your number"))
+
+while (num != sec_num):
+
+    if (num > sec_num):
+
+        print("Too High")
+    else:
+        print("Two Low")
+
+    num = int(input("guess again"))
+print("spot on")
